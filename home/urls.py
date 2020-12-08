6b64b9ca8cbd9eec4e82b9e4183dea3bd433eb24
atharva katre <atharva.katre.71@gmail.com>
2020-12-08 14:45:49 +0530

    package commit

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('about',views.about,name='about'),
     path('package',views.package,name='package'),
     path('contact',views.contact,name='contact'),
+    path('paris',views.paris,name='paris'),
+    path('cyprus',views.cyprus,name='cyprus'),
+    path('los_angeles',views.los_angeles,name='los_angeles'),
+    path('venice',views.venice,name='venice'),
+    path('prague',views.prague,name='prague'),
+    path('tokyo',views.tokyo,name='tokyo'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -9,4 +9,16 @@ def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+def paris(request):
+    return render(request, 'paris.html')
+def cyprus(request):
+    return render(request, 'cyprus.html')
+def venice(request):
+    return render(request, 'venice.html')
+def tokyo(request):
+    return render(request, 'tokyo.html')
+def los_angeles(request):
+    return render(request, 'los_angeles.html')
+def prague(request):
+    return render(request, 'prague.html')
 
